d5b5e1280b19ad3981547a3dadca9570d40dd575
shivam <shivamranjan81@gmail.com>
2021-01-29 22:55:52 +0530

    file completed

---
+++ index.js
@@ -0,0 +1,15 @@
+setInterval(() => {
+    d = new Date();
+    htime=d.getHours();
+    mtime=d.getMinutes();
+    stime=d.getSeconds();
+    hrotation=30*htime+mtime/2;
+
+    mrotation=6*mtime;
+    srotation=6*stime;
+    hourhand.style.transform=`rotate(${hrotation}deg)`;
+    minhand.style.transform=`rotate(${mrotation}deg)`;
+    secondhand.style.transform=`rotate(${srotation}deg)`;
+
+    
+}, 1000);
